208c8b226cba2c21ddbf50e48f3831be8b0877e0
LimitToInfinity <21stLimitless@gmail.com>
2019-10-08 15:10:19 -0600

    Changes to boot and seeds files.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.destroy_all
+
+Comment.create({
+    content: "I lurve this guy!",
+    item_id: 25,
+})
+
+Comment.create({
+    content: "teh best",
+    item_id: 25,
+})
+
+Comment.create({
+    content: "teh worst, i dont want to see this ish",
+    item_id: 1,
+})
